3f057269b2460a92c1e82d52af206f4f5c665aa4
zhoushiya <zhoushiya@126.com>
2019-12-09 11:40:52 +0800

    添加项目文件。

---
+++ AwaitAsyncTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AwaitAsyncTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Main开始:当前线程:" + Thread.CurrentThread.ManagedThreadId);
+            int c = await Task.Run(() =>
+            {
+                Console.WriteLine("Task.Run内部:当前线程:" + Thread.CurrentThread.ManagedThreadId);
+                return 1 + 2;
+            });
+            GetString(c);
+            Console.WriteLine("Main结束:当前线程:" + Thread.CurrentThread.ManagedThreadId);
+            Console.ReadLine();
+        }
+        
+
+        static string GetString(int c)
+        {
+            Console.WriteLine("GetString内部:当前线程:" + Thread.CurrentThread.ManagedThreadId);
+            return c.ToString();
+        }
+    }
+}
